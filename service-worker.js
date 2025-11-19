7598ede7a5b6155699a1277a0907a0afea6d68ad
ahsanrafiquevehari-netizen <ahsanrafiq.uevehari@gmail.com>
2025-11-19 23:12:41 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,27 +7,17 @@ const urlsToCache = [
   './icons/icon-512.png'
 ];
 
-// Install Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Activate Service Worker
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => {
-        if(key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => 
+    Promise.all(keys.map(key => { if(key !== CACHE_NAME) return caches.delete(key); }))
+  ));
 });
 
-// Fetch cached content
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
+
